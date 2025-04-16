c3848bb95b3d66ccda898724faa1ebf44f61a81a
craj100200 <122225396+craj100200@users.noreply.github.com>
2025-04-16 22:30:33 +0530

    Update main.jsx

--- crud001/src/main.jsx
+++ crud001/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Optional, only if you're using dropdowns, modals, etc.
 
 
